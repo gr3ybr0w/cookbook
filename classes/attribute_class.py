5f487933410570e90fccd9bd33746d39a69ee16d
gr3ybr0w <tom.w.heard@googlemail.com>
2018-07-08 12:11:34 +0100

    Add files via upload

---
+++ classes/attribute_class.py
@@ -0,0 +1,36 @@
+# Attribute Critter
+# Demonstrates creating and accessing object attributes
+
+
+# Make a critter class which inherits from object
+class Critter(object):
+
+    """A virtual pet"""
+
+    # sets up the object with it attributes, accepts variables
+    def __init__(self, name):
+        print("A new critter has been born!")
+        self.name = name
+
+    def __str__(self):
+        rep = "Critter object\n"
+        rep += "name: " + self.name + "\n"
+        return rep
+
+    def talk(self):
+        print("Hi.  I'm", self.name, "\n")
+
+# main
+crit1 = Critter("Poochie")
+crit1.talk()
+
+crit2 = Critter("Randolph")
+crit2.talk()
+
+print("Printing crit1:")
+print(crit1)
+
+print("Directly accessing crit1.name:")
+print(crit1.name)
+
+input("\n\nPress the enter key to exit.")
